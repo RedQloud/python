706b516fe95ff5854d4f0d79ce6202d30da75fd8
RedQloud <127067298+RedQloud@users.noreply.github.com>
2023-03-13 16:06:42 +0900

    Update index.py

--- index.py
+++ index.py
@@ -5,35 +5,37 @@ print()
 import cgi
 import cgi, os
 
-# files = os.listdir('data')
-# listStr = ''
-# for item in files:
-#     listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
+files = os.listdir('data')
+listStr = ''
+for item in files:
+    listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
 
-# form = cgi.FieldStorage()
-# if 'id' in form:
-#     pageId = form["id"].value
-#     description = open('data/'+pageId, 'r').read()
-# else:
-#     pageId = 'Welcome'
-#     description = 'Hello, web'
-print('''<!doctype html>
-<html>
-<head>
-  <title>WEB1 - Welcome</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="index.py">WEB</a></h1>
-  <ol>
-    <li><a href="index.py?id=HTML">HTML</a></li>
-    <li><a href="index.py?id=CSS">CSS</a></li>
-    <li><a href="index.py?id=JavaScript">JavaScript</a></li>
-    <li><a href="index.py?id=Python">Python</a></li>
-    {listStr}
-  </ol>
-  <h2>{title}</h2>
-  <p>{desc}</p>
-</body>
-</html>
+form = cgi.FieldStorage()
+if 'id' in form:
+    pageId = form["id"].value
+    description = open('data/'+pageId, 'r').read()
+else:
+    pageId = 'Welcome'
+    description = 'Hello, web'
+    
+print('''
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - Welcome</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="index.py">WEB</a></h1>
+    <ol>
+      <li><a href="index.py?id=HTML">HTML</a></li>
+      <li><a href="index.py?id=CSS">CSS</a></li>
+      <li><a href="index.py?id=JavaScript">JavaScript</a></li>
+      <li><a href="index.py?id=Python">Python</a></li>
+      {listStr}
+    </ol>
+    <h2>{title}</h2>
+    <p>{desc}</p>
+  </body>
+  </html>
 '''.format(title=pageId, desc=description, listStr=listStr))
